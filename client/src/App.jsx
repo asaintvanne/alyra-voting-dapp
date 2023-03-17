ed08d5e70561bb6ad6d8422f3ae9f06705e88ccd
Adrien Rochedy <adrien.rochedy@gmail.com>
2023-03-17 18:46:19 +0100

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <EthProvider>
       <div id="App">
-      < ToastContainer />
+      <ToastContainer />
         <div className="container">
     
           <AccountInfo />
